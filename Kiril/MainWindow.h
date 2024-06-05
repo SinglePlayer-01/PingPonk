d19872e105a53d4dac7b1f0e10072bb6e99ebeb5
SinglePlayer-01 <157644774+SinglePlayer-01@users.noreply.github.com>
2024-06-05 15:48:28 +0300

    Добавьте файлы проекта.

---
+++ Kiril/MainWindow.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<Windows.h>
+
+#include"BaseWindow.h"
+
+class MainWindow : public BaseWindow<MainWindow>
+{
+public:
+    PCWSTR  ClassName() const { return L"Sample Window Class"; }
+    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+}; 
+
